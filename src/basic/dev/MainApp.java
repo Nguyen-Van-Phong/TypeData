3dff06589a72d4cb6021d7c75b1f74c8453adb5c
phongz <phongnguyenzzz991@gmai.com>
2021-10-20 15:40:55 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,48 @@
+package basic.dev;
+ 
+public class MainApp {
+	public static void main(String[] args) {
+		/**
+		 *thong tin mot sinh vien 
+		 */
+		String ten= "nguyen van phong";
+		System.out.println("ho va ten : "+ ten);
+		String msv="2160171";
+		System.out.println("ma sinh vien : "+ msv);
+		String cmnd="192033609";
+		System.out.println("chung minh nhan dan : "+ cmnd);
+		String tl= "21CDTH41";
+		System.out.println("ten lop : "+ tl);
+		String tt="Cao Dang CN Hue";
+		System.out.println("ten truong : "+ tt);
+		String dtb="9";
+		System.out.println("diem trung binh : "+ dtb);
+		/**
+		 * thong tin cong nhan
+		 */
+		String teen="phong";
+		System.out.println("teen : "+ ten);
+		String dc="hue";
+		System.out.println("dia chi : "+ dc);
+		String sdt="123456789";
+		System.out.println("so dien thoai : "+ sdt);
+		String hsl="334";
+		System.out.println("he so luong : "+ hsl);
+		/**
+		 * thong tin hang chiec xe
+		 */
+		String teeen="sh150";
+		System.out.println("ten : " + teeen);
+		String hang="webu";
+		System.out.println("hang : "+hang );
+		String gia="450000000";
+		System.out.println("gia : "+ gia);
+		String ms="vang";
+		System.out.println("mau son : "+ ms);
+		
+				
+		
+		
+		
+	}
+}
